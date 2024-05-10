6de464278201a97c13da606169efa3c1e1c8b249
Artem Pokatilov <dr.artpokatilov@yandex.ru>
2024-05-10 12:06:50 +0300

    Added logical functional for move figures between cells

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -4,13 +4,17 @@ import { Cell } from "../models/Cell";
 interface CellProps {
   cell: Cell;
   selected: boolean;
+  click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({cell, selected}) => {
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
   return (
     <div 
-    className={['cell', cell.color, selected ? "selected" : ''].join(' ')}
+      className={['cell', cell.color, selected ? "selected" : ''].join(' ')}
+      onClick={() => click(cell)}
+      style={{background: cell.available && cell.figure ? 'green' : ''}}
     >
+      {cell.available && !cell.figure && <div className={"available"}/>}
       {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
     </div>
   );
